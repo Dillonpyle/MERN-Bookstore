5cccf5ebc509b2f9375a34cde7c303519d586d17
Dillon Pyle <dillonpyle@Dillons-MBP.lan>
2024-12-02 17:31:13 -0600

    first commit

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from 'mongoose';
+
+const app =  express();
+
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('Welome to MERN Stack')
+});
+
+
+app.listen(PORT, () => {
+    console.log(`App is listening to port: ${PORT}`);
+});
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+
+    })
+    .catch((error) => {
+        console.log(error);
+    });
