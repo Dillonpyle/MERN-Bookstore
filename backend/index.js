fc7c4951e0746e173c4cb715b14703e74bc841c2
Dillonpyle <dillonpyle1991@gmail.com>
2024-12-04 16:27:51 -0600

    added route to delete book with mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express, { request } from "express";
+import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
@@ -97,6 +97,23 @@ app.put('/books/:id', async (request, response) => {
     }
 });
 
+//Route to delete a book
+app.delete('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndDelete(id);
+
+        if (!result){
+            return response.status(404).json({ message: 'Book not found'});
+        }
+
+        return response.status(200).send({message: 'Book deleted successfully'});
+    } catch (error){
+        console.log(error.message);
+        response.status(500).send({ message: error.message});
+    }
+});
 
 mongoose
     .connect(mongoDBURL)
