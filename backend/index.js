12e543006ab1272a42970aab94644fee623b52cb
Dillonpyle <dillonpyle1991@gmail.com>
2024-12-04 17:09:30 -0600

    create react app, Vite, and Tailwind.css

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,16 @@
 import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
-import { Book } from './models/bookModel.js';
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app =  express();
 
 //Middleware for parsing request body
 app.use(express.json());
+
+//Middleware for handling CORS policy
+app.use(cors());
 
 app.get('/', (request, response) => {
     console.log(request)
